064d0a27d62bb34dfccefe0edf8cf0603518301e
Moneky <officialsunday@protonmail.com>
2025-12-28 17:26:11 +0000

    udp and tcp

--- Main.hx
+++ Main.hx
@@ -1,17 +1,35 @@
 import sys.net.Socket;
 import sys.net.Host;
+import sys.net.UdpSocket;
 
 class Main {
     static function main() {
+        // Socket declare
         var socket:Socket = new Socket();
+        var udp = new UdpSocket();
 
-        var host:Host = new Host("127.0.0.1"); 
+        // Host
+        var host:Host = new Host("127.0.0.1");
+
+        // addr for UDP
+        var addr = new sys.net.Address();
+        addr.host = host.ip;
+
+        // Connect and bind
+        udp.bind(host, 9999); 
         socket.connect(host, 80);
 
+        // sending the data to TCP
         var out = socket.output;
         out.writeString("hello tcp world\n");
         out.flush();
 
+
+        // sending the data to UDP
+        var msg = haxe.io.Bytes.ofString("DISCOVER");
+        udp.sendTo(msg, 0, msg.length, addr);
+
+        // close tcp socket, UDP doesnt need this
         socket.close();
     }
 }
